50aa716dcf140b9a9550620b839d93eede66346c
Carlos <carlos.santos112@outlook.com>
2019-12-09 22:33:06 -0800

    Update
    The adding numbers part of the script is working.  I need to find out how to copy and replace files in python.

--- add_remove_line_numbers.py
+++ add_remove_line_numbers.py
@@ -15,8 +15,29 @@ while(i !=1):
     if(option == 1):
         file=input("Enter File Name: ") #read in the file name
         print("File name entered: ",file) # this is kinda like C
-        f = open(file,a
+        f = open(file,"r") # see for more information: https://www.w3schools.com/python/python_file_handling.asp
+        #Note f is the file pointer
+        print("Original File: \n")
+        print(f.read())
+        f.close()
         print("Adding line numbers...\n")
+        # reference material for code below
+        # https://docs.python.org/2/tutorial/inputoutput.html
+        # https://docs.python.org/2/library/functions.html#enumerate
+        with open(file, "r") as openfile:
+            with open("test2.txt", "w") as outfile:
+                for line_number, line in enumerate(openfile):
+                    outfile.write('{0:<3}{1}'.format(line_number+1, line))
+        f.close()
+        f.close()
+
+        f = open("test2.txt","r")
+        print("\nModified File: \n")
+        print(f.read())
+        f.close()
+        print("\n")
+        
+        
     elif (option == 2):
         file=input("Enter File Name: ") #read in the file name
         print("File name entered: ",file)
